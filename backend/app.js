809d574fbfc6dce2411dde10b97c026c87d609f5
Sapphire <sreem7808@gmail.com>
2026-02-28 00:04:49 +0530

    Make changes to index.js related to cors

--- backend/app.js
+++ backend/app.js
@@ -17,11 +17,23 @@ const server = createServer(app);
 
 initializeSocket(server);
 
+const allowedOrigins = process.env.CORS_ORIGINS
+  ? process.env.CORS_ORIGINS.split(",")
+  : [];
+
 app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials: true,
-    })
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
 );
 
 app.use("/api/v1/webhook", webhookRouter);
