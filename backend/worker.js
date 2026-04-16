1dea68520aabb624077a714e26c0ed6749812124
Sapphire <sreem7808@gmail.com>
2026-04-16 14:21:43 +0530

    Add Express server to worker for Render free tier compatibility

--- backend/worker.js
+++ backend/worker.js
@@ -1,10 +1,22 @@
+import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-await mongoose.connect(process.env.MONGODB_URI);
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Worker is running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Worker HTTP server running on ${PORT}`);
+});
+
+// DB + worker
+await mongoose.connect(process.env.MONGODB_URI);
 console.log("Worker DB connected");
 
 import "./jobs/aiWorker.js";
