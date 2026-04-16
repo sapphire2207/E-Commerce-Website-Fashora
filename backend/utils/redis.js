18cd21defb25ba74d43ceb76b49b7c7085f1c2b8
Sapphire <sreem7808@gmail.com>
2026-04-16 14:11:25 +0530

    Fix Redis SSL connection issue Part 2

--- backend/utils/redis.js
+++ backend/utils/redis.js
@@ -1,13 +1,10 @@
 import { Redis } from "ioredis";
 
-export const redisConnection =
-  process.env.REDIS_URL
-    ? new Redis(process.env.REDIS_URL, {
-        maxRetriesPerRequest: null,
-        tls: {},
-      })
-    : new Redis({
-        host: "127.0.0.1",
-        port: 6379,
-        maxRetriesPerRequest: null,
-      });
+export const redisConnection = new Redis(
+  process.env.REDIS_URL || {
+    host: "127.0.0.1",
+    port: 6379,
+  },
+  {
+    maxRetriesPerRequest: null,
+});
